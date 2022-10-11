46ea3d3c9cd574aa68b9a858bb18d8cdc0e2995a
victorgis <essangvictor@gmail.com>
2022-10-11 14:13:46 +0100

    built weather app

--- apps/weather-app.js
+++ apps/weather-app.js
@@ -14,7 +14,6 @@ let btn = document.getElementById('btn').addEventListener("click", function(even
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityNames}&limit=5&appid=ee914cdd33ab3acb4346421f7daf6fd4`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
             let datay = data[0].lat;
             let datax = data[0].lon;
 
